e053da2023a46e9a9db87e4b6c93d878cbb3924f
sergei.lukyanov <s.lukyanov@swemel.ru>
2018-04-19 19:32:23 +0400

    append BasePage and use MainMenu

--- page_elements/MainMenu.py
+++ page_elements/MainMenu.py
@@ -3,10 +3,18 @@ from Button import Button
 
 
 class MainMenu(object):
-    locator = "xpath=//ul[@class='nav-menu']"
+    logo_link = "xpath//a[@rel='home']"
 
-    home_button = Button(locator + "//a[text()='Главная']")
-    blog_button = Button(locator + "//a[text()='Блог']")
-    java_button = Button(locator + "//a[text()='Java\C# & WebDriver']")
-    literature = Button(locator + "//a[text()='Литература']")
-    site_map_button = Button(locator + "//a[text()='Карта сайта']")  # //ul[@class='nav-menu']//a[text()='Карта сайта']
+    locator_menu = "xpath=//ul[@class='nav-menu']"
+    home_button = Button(locator_menu + "//a[text()='Главная']")
+    blog_button = Button(locator_menu + "//a[text()='Блог']")
+    java_button = Button(locator_menu + "//a[text()='Java\C# & WebDriver']")
+    literature = Button(locator_menu + "//a[text()='Литература']")
+    site_map_button = Button(locator_menu + "//a[text()='Карта сайта']")
+    # //ul[@class='nav-menu']//a[text()='Карта сайта']
+    search_button = "xpath=//div[@class='search-toggle']"
+
+    # ---  search form  ---
+    search_form = "xpath=//div[@id='search-box']"
+    search_input = search_form + "//input[@type='search']"
+    submit_button = search_form + "//input[@type='submit']"
--- page_objects/IndexPage.py
+++ page_objects/IndexPage.py
@@ -1,2 +1,8 @@
 # -*- coding: utf-8 -*-
 
+from page_elements.MainMenu import MainMenu
+from BasePage import BasePage
+
+
+class IndexPage(BasePage):
+    header = MainMenu()
